a19946c36083dfe332a9f856caaa9fee6956fe5d
Benjamin Schäfer <benibschafer@gmail.com>
2024-03-20 13:42:54 +0000

    deployment changes

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('.datepicker').datepicker(
+    {
+      format: 'yyyy-mm-dd',
+      showClearBtn: true,
+      i18n: {
+        done: 'Select'
+      }
+    }
+  );
+  $('select').formSelect();
+  $('.collapsible').collapsible();
+});
