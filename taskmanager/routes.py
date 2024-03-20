613fe672258097894cf928b9c3b935f565118c11
Benjamin Schäfer <benibschafer@gmail.com>
2024-03-20 10:38:50 +0100

    add create and read functionality

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -9,7 +9,8 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html" , categories = categories)
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
@@ -17,5 +18,6 @@ def add_category():
         category = Category(category_name = request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
+        print("Category added successfully")
         return redirect(url_for("categories"))
     return render_template("add_category.html")
